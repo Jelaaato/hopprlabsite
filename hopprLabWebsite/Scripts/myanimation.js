2ce8ce8193be15d16b995f1a2f708dcbb754dacc
jelaaato <erjel88@yahoo.com>
2017-05-17 16:38:36 +0800

    Initial Commit

---
+++ hopprLabWebsite/Scripts/myanimation.js
@@ -0,0 +1,10 @@
+﻿var fadeIn = TweenMax.to('#webicon > img', .375, { opacity: 1 });
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: "#web",
+    triggerHook: "onEnter",
+    reverse: true
+})
+.setTween(fadeIn)
+//.duration(400)
+.addTo(controller);
