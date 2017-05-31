d5a927fa8a589494656787901ad58fe571f88572
jelaaato <erjel88@yahoo.com>
2017-05-31 16:00:56 +0800

    Recent Update

---
+++ hopprLabWebsitev2/Controllers/SectionsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hopprLabWebsitev2.Controllers
+{
+    public class SectionsController : Controller
+    {
+        // GET: Section
+        public ActionResult Web()
+        {
+            return View();
+        }
+
+        public ActionResult Mobile()
+        {
+            return View();
+        }
+
+        public ActionResult Analytics()
+        {
+            return View();
+        }
+
+        public ActionResult IoT()
+        {
+            return View();
+        }
+    }
+}
