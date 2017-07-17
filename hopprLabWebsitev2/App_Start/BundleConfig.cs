affc6786ef87ca8c4d2cf0823a7c82f1f660ffad
jelaaato <erjel88@yahoo.com>
2017-07-17 13:37:49 +0800

    Fix 'Developers' Collapsible' for mobile devices

---
+++ hopprLabWebsitev2/App_Start/BundleConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace hopprLabWebsitev2.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jQuery/jquery-3.1.1.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-2.6.2.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                   "~/Scripts/typeeffect.js",
+                   "~/Scripts//slick/slick.min.js",
+                   "~/Scripts/scrollreveal.min.js",
+                   "~/Scripts/typeeffect.js",
+                   "~/Scripts/app.js"
+           ));
+
+           bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                    "~/Scripts/bootstrap/bootstrap.min.js"
+           ));
+
+           bundles.Add(new StyleBundle("~/content/css").Include(
+                   "~/Content/bootstrap/bootstrap.min.css",
+                   "~/Content/font-awesome.min.css",
+                   "~/Content/slick/slick.css",
+                   "~/Content/slick/slick-theme.css",
+                   "~/Content/Site.css"
+           ));
+        }
+    }
+}
--- hopprLabWebsitev2/Global.asax.cs
+++ hopprLabWebsitev2/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using hopprLabWebsitev2.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace hopprLabWebsitev2
@@ -13,6 +15,7 @@ namespace hopprLabWebsitev2
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
